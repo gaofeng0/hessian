508b27b7711c972485dcc04c6412525e3c683a7a
高峰 <634728467@qq.com>
2019-10-17 19:44:52 +0800

    dev 1

--- src/main/java/com/gaofeng/hessian/web/Test.java
+++ src/main/java/com/gaofeng/hessian/web/Test.java
@@ -3,7 +3,6 @@ package com.gaofeng.hessian.web;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("branch111 ");
         System.out.println("master");
     }
 }
