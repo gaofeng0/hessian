157fb706076cb52e2021944755a73afbcf69b978
高峰 <634728467@qq.com>
2019-10-18 13:48:06 +0800

    dev2  3

--- src/main/java/com/gaofeng/hessian/web/TestB.java
+++ src/main/java/com/gaofeng/hessian/web/TestB.java
@@ -4,5 +4,6 @@ public class TestB {
     public static void main(String[] args) {
         System.out.println("dev2");
         System.out.println("dev2");
+        System.out.println("dev2");
     }
 }
