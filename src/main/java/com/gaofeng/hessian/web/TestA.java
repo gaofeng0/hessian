70e6968dd44a61d88536eb7d04c8bf674fc16667
高峰 <634728467@qq.com>
2019-10-18 09:19:37 +0800

    qqq

--- src/main/java/com/gaofeng/hessian/web/TestA.java
+++ src/main/java/com/gaofeng/hessian/web/TestA.java
@@ -4,5 +4,6 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("aaadsss");
         System.out.println("aaadsss");
+        System.out.println("aaadsss");
     }
 }
