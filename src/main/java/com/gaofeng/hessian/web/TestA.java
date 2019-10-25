d6bf913967ad872574e5f59a9ccaf1764b97d8c1
高峰 <634728467@qq.com>
2019-10-25 08:27:29 +0800

    test

--- src/main/java/com/gaofeng/hessian/web/TestA.java
+++ src/main/java/com/gaofeng/hessian/web/TestA.java
@@ -2,7 +2,7 @@ package com.gaofeng.hessian.web;
 
 public class TestA {
     public static void main(String[] args) {
-        System.out.println("dev1");
+        System.out.println("dev1 2");
         System.out.println("dev2    ");
     }
 }
